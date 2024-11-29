f7e44cad9446a87ee102e31bfde982de395e5af3
AXKuhta <11133304+AXKuhta@users.noreply.github.com>
2024-11-29 22:57:29 +0800

    Trajectory prediction

--- Scenes/sling.gd
+++ Scenes/sling.gd
@@ -4,6 +4,7 @@ extends StaticBody2D
 @onready var draggable_base = draggable.position
 @onready var rope_a:Line2D = $RopeA
 @onready var camera = $"../Camera2D"
+var taking_input:bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,11 +14,22 @@ func _unhandled_input(event: InputEvent) -> void:
 	if event.is_echo():
 		return
 
-	if event is InputEventMouseButton and event.is_released():
+	if event is InputEventMouseButton and event.is_released() and taking_input:
 		if event.button_index == MOUSE_BUTTON_LEFT:
 			var launch_vector = draggable_base - draggable.position
 			
 			$"../BirbFactory".fire_bird(draggable.global_position, launch_vector*10)
+
+# 
+func plot_trajectory(vec):
+	var gravity = Vector2(0, 9.8)
+	var lst = []
+	
+	for i in range(20):
+		lst.append(draggable_base + vec - vec*i + gravity*i*i/2)
+		
+	$Trajectory.points = lst
+
 
 # Обработка непрерывного ввода
 # TODO: Привязать верёвки к точкам корзины?
@@ -27,14 +39,26 @@ func _process(delta: float) -> void:
 		var local_mouse = get_local_mouse_position()
 		var vec = local_mouse - draggable_base
 		
-		camera.scroll_locked = true
-		camera.position = self.position
+		if vec.length() <= 100:
+			taking_input = true
 		
-		vec = vec.limit_length(100)
-		draggable.position = draggable_base + vec
-		draggable.rotation = atan2(vec.y, vec.x) - PI
-		
-		rope_a.points[1] = draggable_base + vec
+		if taking_input:
+			camera.scroll_locked = true
+			camera.position = self.position
+			
+			vec = vec.limit_length(100)
+			draggable.position = draggable_base + vec
+			draggable.rotation = atan2(vec.y, vec.x) - PI
+			
+			plot_trajectory(vec)
+			
+			rope_a.points[1] = draggable_base + vec
 	else:
 		draggable.position = draggable_base
 		rope_a.points[1] = draggable_base
+		
+		$Trajectory.points = []
+		
+		if taking_input:
+			taking_input = false
+			camera.scroll_locked = false
