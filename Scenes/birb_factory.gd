0e4e9d8c0f6ff69dff40189c5ff8e91224b085eb
AXKuhta <11133304+AXKuhta@users.noreply.github.com>
2024-11-29 19:40:00 +0800

    Basic camera follow

--- Scenes/birb_factory.gd
+++ Scenes/birb_factory.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @export var Birb: PackedScene = preload("res://Scenes/bird.tscn")
+var active_birb:Node2D
 
 func fire_bird(origin, vector):
 		var birb:RigidBody2D = Birb.instantiate()
@@ -8,12 +9,14 @@ func fire_bird(origin, vector):
 		birb.linear_velocity = vector
 		print("Fire with", vector.length(), "velocity")
 		add_child(birb)
+		
+		active_birb = birb
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-		pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-		pass
+	if active_birb:
+			$"../Camera2D".position = active_birb.global_position
