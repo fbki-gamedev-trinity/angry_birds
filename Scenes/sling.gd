c46042f0fe9e43191563218da9ba9f4bd38f9571
AXKuhta <11133304+AXKuhta@users.noreply.github.com>
2024-12-06 23:37:24 +0800

    Add some offset to the bird when slinging

--- Scenes/sling.gd
+++ Scenes/sling.gd
@@ -4,13 +4,16 @@ extends StaticBody2D
 @onready var rope:Line2D = $Rope
 @onready var draggable_base = rope.points[1]
 @onready var camera = $"../Camera2D"
+@onready var birb_factory = $"../BirbFactory"
 @onready var bird:Node2D = $Bird
+
 var taking_input:bool = false
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
+# Детектировать отпускание левой кнопки мыши
+# и запускать птичку, если рогатка натянута
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_echo():
 		return
@@ -19,7 +22,7 @@ func _unhandled_input(event: InputEvent) -> void:
 		if event.button_index == MOUSE_BUTTON_LEFT:
 			var launch_vector = draggable_base - rope.points[1]
 			
-			$"../BirbFactory".fire_bird(rope.points[1] + rope.global_position, launch_vector*10)
+			birb_factory.fire_bird(bird.global_position, launch_vector*10)
 
 # Предсказать траекторию полёта
 func plot_trajectory(vec):
@@ -27,14 +30,15 @@ func plot_trajectory(vec):
 	var lst = []
 	
 	for i in range(30):
-		lst.append(draggable_base + vec - vec*i + gravity*i*i/2)
+		lst.append(bird.position - vec*i + gravity*i*i/2)
 		
 	$Trajectory.points = lst
 
-# Обработка непрерывного ввода
+# Здесь:
+# - Натягивание рогатки
+# - Позиционирование птички
+# - Контроль видимости птички
 func _process(delta: float) -> void:
-	bird.visible = $"../BirbFactory".active_birb == null
-	
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 		var local_mouse = get_local_mouse_position()
 		var vec = local_mouse - draggable_base
@@ -46,15 +50,21 @@ func _process(delta: float) -> void:
 			camera.scroll_locked = true
 			camera.position = self.position
 			
+			
 			vec = vec.limit_length(100)
-			bird.position = draggable_base + vec
+			
+			# Добавочка чтобы сдвинуть птичку чуть вверх
+			var offset = vec.rotated(PI/2).normalized()*20
+			
+			bird.position = draggable_base + vec + offset
 			bird.rotation = atan2(vec.y, vec.x) - PI
 			
 			plot_trajectory(vec)
 			
 			rope.points[1] = draggable_base + vec
 	else:
-		bird.position = draggable_base
+		bird.position = draggable_base + Vector2(0, -20)
+		bird.rotation = 0
 		rope.points[1] = draggable_base
 		
 		$Trajectory.points = []
@@ -62,3 +72,5 @@ func _process(delta: float) -> void:
 		if taking_input:
 			taking_input = false
 			camera.scroll_locked = false
+			
+	bird.visible = birb_factory.active_birb == null
