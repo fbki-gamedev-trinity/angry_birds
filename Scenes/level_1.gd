9ccf4056139ccfa4567d98e7b0d044a00cb2b372
AXKuhta <11133304+AXKuhta@users.noreply.github.com>
2024-12-14 11:24:31 +0800

    Add playable area and remove birds that left the playable area

--- Scenes/level_1.gd
+++ Scenes/level_1.gd
@@ -22,3 +22,9 @@ func _process(delta: float) -> void:
 	if $Sling.shots == 0:
 		await get_tree().create_timer(3).timeout
 		$GameOver.invoke("Поражение!")
+
+
+func _on_playable_area_body_exited(body: Node2D) -> void:
+	if body == $BirbFactory.active_birb:
+		await get_tree().create_timer(3).timeout
+		$BirbFactory.remove_bird()
